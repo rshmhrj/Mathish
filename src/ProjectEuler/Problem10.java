617a4f47dbf073126db789025a998f37abe71774
Rishi Maharaj <dudewheysmehemail@gmail.com>
2012-02-19 20:38:17 -0500

    Sunday work

---
+++ src/ProjectEuler/Problem10.java
@@ -0,0 +1,35 @@
+package ProjectEuler;
+import java.util.Scanner;
+
+public class Problem10 {
+	
+	/**
+	 * @param args
+	 * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+	 * Find the sum of all the primes below two million.
+	 * 2,000,000
+	 * 2000000
+	 */
+	
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("This program will find the sum of all the primes below x.");
+		System.out.print("Please enter a value for x:");
+		int num = input.nextInt();
+		long start = System.currentTimeMillis();
+		
+		
+		
+		
+		
+		
+		
+		
+		long end = System.currentTimeMillis();
+		int time = (int)(end - start);
+		System.out.println("****The Program took "+time+"ms to execute.****");
+		System.out.println("End of Program.");
+	} // end method main
+
+} // end class ProblemTemplate
